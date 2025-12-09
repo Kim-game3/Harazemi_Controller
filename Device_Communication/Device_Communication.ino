65c77aa69cda25c8434a0fc9ca7a3f2889b4d8b2
Kim-game3 <zweilous387@icloud.com>
2025-12-09 17:15:53 +0900

    Device側のプロジェクトも入れた

--- Device_Communication/Device_Communication.ino
+++ Device_Communication/Device_Communication.ino
@@ -1,17 +1,16 @@
-//XIAOのMacアドレス:10:51:db:1a:c0:fc
+//Slave側
+//M5StampのMacアドレス:34:b7:da:f2:2a:8c
 
 #include"ESPNowEz.h"
 
-CESPNowEZ espnow(0);
+CESPNowEZ espnow(1);
 
-void setup()
-{
+void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
 }
 
-void loop()
-{
+void loop() {
   // put your main code here, to run repeatedly:
   Serial.println(espnow.GetMacAddrChar());
   delay(1000);
