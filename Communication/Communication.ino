9e8582e62e5881dca95bc1596580c8ef207de997
Kim-game3 <zweilous387@icloud.com>
2025-12-13 15:44:37 +0900

    とりあえず通信のセットアップをした

--- Communication/Communication.ino
+++ Communication/Communication.ino
@@ -4,15 +4,44 @@
 
 CESPNowEZ espnow(0);
 
+uint8_t DeviceMacAddr[] = {0x34, 0xb7, 0xda, 0xf2, 0x2a, 0x8c };
+
+ESPNOW_Con2Devdata ControllerData;
+
+int outputFlag;
+
+void onDataReceived(const esp_now_recv_info* info, const uint8_t* data, int data_len)
+{
+  //受信時の処理を書く
+  memcpy(&deviceData, data, data_len);
+  outputFlag = 1;
+}
+
 void setup()
 {
   // put your setup code here, to run once:
+  espnow.Initialize(OnDataReceived);
+
+  espnow.SetDeviceMacAddr(DeviceMacAddr);
+
+  outputFlag = 0;
+
   Serial.begin(115200);
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
-  Serial.println(espnow.GetMacAddrChar());
-  delay(1000);
+    if(Serial.available() > 0)
+  {
+    inputChar = Serial.read();
+    controllerData.cmd = inputChar;
+    espnow.Send(1, &controllerData, sizeof(controllerData)); // id:1に送る
+  }
+
+  if(outputFlag)
+  {
+    outputFlag = 0;
+    Serial.printf("SE\n", );
+  }
 }
--- Device_Communication/Device_Communication.ino
+++ Device_Communication/Device_Communication.ino
@@ -5,13 +5,32 @@
 
 CESPNowEZ espnow(1);
 
+ESPNOW_Dev2ConData DeviceData;
+
+uint8_t ControllerAddress[] = {0x10, 0x51, 0xdb, 0x1a, 0xc0, 0xfc};
+
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
+{
+  // 送信完了時の処理
+}
+
+void OnDataReceived(const uint8_t* mac_addr, const uint8_t* data, int data_len)
+{
+   // 受信時の処理
+}
+
 void setup() {
   // put your setup code here, to run once:
+  espnow.Initialize(OnDataReceived, OnDataSent);
+
+  espnow.SetControllerMacAddr(ControllerAddress);
+
   Serial.begin(115200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println(espnow.GetMacAddrChar());
-  delay(1000);
+  espnow.Send(&deviceData, sizeof(deviceData));
+
+  delay(10);
 }
